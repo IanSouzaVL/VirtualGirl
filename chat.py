c8faec8ed52c3255520105fc3e8e5182ec15bad3
IanSouzaVL <iansouzavansed2@gmail.com>
2026-07-21 22:02:06 -0300

    Cores e conversa persistente adicionadas

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 from utilitarios import *
 import json, requests
-url = "http://localhost:11434/api/chat"
+# url = "http://10.234.184.44:11434/api/chat"
+url = "http://192.168.18.9:11434/api/chat"
 model = "llama3.1:8b-instruct-q4_K_M"
 
 with open("mensagens.json", "r", encoding="utf-8") as arquivo:
@@ -15,26 +16,27 @@ dados = {
     "stream": False
 }
 
-user_content = input("Digite uma mensagem: ")
+while True:
+    user_content = input("\033[32mConverse com Fern: \033[0m")
+    print()
 
+    dados["messages"].append(
+        {
+            "role": "user",
+            "content": user_content
+        }
+    )
 
-dados["messages"].append(
-    {
-        "role": "user",
-        "content": user_content
-    }
-)
+    try:
+        r = requests.post(url, json=dados, timeout=60)
+        r.raise_for_status()
 
-try:
-    r = requests.post(url, json=dados, timeout=60)
-    r.raise_for_status()
+        ia_message_dict = r.json()["message"]
 
-    ia_message_dict = r.json()["message"]
+        mensagens.append(ia_message_dict)
 
-    mensagens.append(ia_message_dict)
+        write_json(mensagens)
+        print(f"\033[38;2;171;71;188m{ia_message_dict["content"]}\033[0m\n")
 
-    write_json(mensagens)
-    print(ia_message_dict["content"])
-
-except requests.RequestException as e:
-    print(f"Erro: {e}")
+    except requests.RequestException as e:
+        print(f"Erro: {e}")
